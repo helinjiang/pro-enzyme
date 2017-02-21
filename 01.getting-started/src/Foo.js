0cfefbf7433045059df297c0eb35b7036dd2b664
helinjiang <me@helinjiang.com>
2017-02-21 11:05:41 +0800

    add .babelrc

---
+++ 01.getting-started/src/Foo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Foo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="foo"/>
+        );
+    }
+}
+
+export default Foo;
